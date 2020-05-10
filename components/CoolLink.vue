982b6605cee35a5ddf54fb8e628f6b43622d8f4b
Mattia Sinisi <mattia.sinisi@virgilio.it>
2020-05-10 11:45:24 +0200

    Link + Icon : Wrapper component

---
+++ components/CoolLink.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="level">
+    <div class="level-left">
+      <div class="level-item">
+        <component :is="component" class="is-block" :to="to" :href="to" noopener>
+          <span  class="has-text-centered">
+            <b-icon v-if="icon" :icon="icon"></b-icon>
+            <span>
+              <slot></slot>
+            </span>
+          </span>
+        </component>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    icon: String,
+    to: String,
+    internal: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    component(){
+      return this.internal && this.to ? 'nuxt-link' : 'a'
+    }
+  }
+}
+</script>
