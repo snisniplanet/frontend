2dfc275dad2754868b60baace534430c8cecf11e
Mattia Sinisi <mattia.sinisi@virgilio.it>
2020-05-13 12:48:43 +0200

    Error page

---
+++ layouts/error.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hero is-large is-info">
+    <div class="hero-head container">
+      <p class="title is-0">{{error.statusCode}}</p>
+    </div>
+
+    <div class="hero-head container section">
+      <p class="subtitle is-2">{{error.message}}</p>
+      <p class="subtitle is-4">
+        <span>Our awesome site</span>
+        <span class="has-text-warning">{{error.path}}</span>
+      </p>
+    </div>
+
+    <div class="hero-foot container section">
+      <nuxt-link to="/">
+        <span class="button is-large">Back to the homepage!</span>
+      </nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['error']
+}
+</script>
