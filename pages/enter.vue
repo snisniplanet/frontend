0bf0b29a1510c2f2370b7997660a09b1cf96fad7
Mattia Sinisi <mattia.sinisi@virgilio.it>
2020-05-15 23:10:09 +0200

    Created Login / Register page

---
+++ pages/enter.vue
@@ -0,0 +1,93 @@
+<template>
+  <!--TODO Fix missing icons -->
+  <div class="container">
+    <b-tabs expanded multiline>
+      <b-tab-item label="Login">
+        <form class="form" @submit.prevent="">
+          <div class="field">
+            <label class="label">Your email / Username</label>
+            <div class="control">
+              <input
+                class="input"
+                type="text"
+              />
+            </div>
+          </div>
+
+          <div class="field">
+            <label class="label">Your password</label>
+            <div class="control">
+              <input
+                class="input"
+                type="password"
+                placeholder="********"
+              />
+            </div>
+          </div>
+
+          <div class="field">
+            <div class="control">
+              <b-button
+                class="button is-large"
+                native-type="submit"
+                icon-left="github"
+              >
+                Enter
+              </b-button>
+            </div>
+          </div>
+        </form>
+      </b-tab-item>
+
+      <b-tab-item label="Register">
+        <form class="form" @submit.prevent="">
+          <div class="field">
+            <label class="label">Your <span class="has-text-success">new</span> username</label>
+            <div class="control">
+              <b-input
+                icon-pack="fas"
+                icon="at"
+                type="text"
+              />
+            </div>
+          </div>
+
+          <div class="field">
+            <label class="label">Your email</label>
+            <div class="control">
+              <b-input
+                icon="email"
+                type="email"
+                placeholder="your@awesome.email"
+              />
+            </div>
+          </div>
+
+          <div class="field">
+            <label class="label">Your <span class="has-text-success">new</span> password</label>
+            <div class="control">
+              <input
+                class="input"
+                type="password"
+                placeholder="********"
+              />
+            </div>
+          </div>
+
+          <div class="field">
+            <div class="control">
+              <b-button
+                class="button is-large"
+                native-type="submit"
+                icon-pack="fas"
+                icon-right ="arrow-circle-right"
+              >
+                Welcome!
+              </b-button>
+            </div>
+          </div>
+        </form>
+      </b-tab-item>
+    </b-tabs>
+  </div>
+</template>
