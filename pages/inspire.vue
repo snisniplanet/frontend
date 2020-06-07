835c579a99f9d54941f5733809fae8d3e895e5f2
Mattia Sinisi <mattia.sinisi@virgilio.it>
2020-06-07 22:47:46 +0200

    Removed broken styling

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -151,8 +151,6 @@ $section5-bg-color: $light;
 
 .section-bubble5 {
   background: $section5-bg-color;
-
-  text-align: center;
 }
 
 //$bubbles-a: '<svg version="1.2" baseProfile="tiny" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1185 248"><g fill="#FF8787"><circle cx="76" cy="121.1" r="20"/><circle cx="870" cy="201.1" r="11"/><circle cx="814.5" cy="165.6" r="24.5"/><path d="M0 0v17.7c22.7 14.8 53 31.9 90.7 51.5 150.8 78 322 116.6 424.8 69.3 102.9-47.4 138-69.3 210.8-69.3s118.3 48.6 219.5 38.3 76.3-59.3 188.7-59.3c18.9 0 35.5 2.6 50.5 6.8V0H0z"/></g></svg>';
