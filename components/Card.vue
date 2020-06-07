e2f6a5c50b3310a1b3dc68674df7dc750c0f46e4
Mattia Sinisi <mattia.sinisi@virgilio.it>
2020-06-07 22:22:13 +0200

    New Card component

--- components/Card.vue
+++ components/Card.vue
@@ -1,23 +1,32 @@
 <template>
-  <div class="column">
-    <div class="card">
-      <header class="card-header">
-        <p class="card-header-title has-text-grey">
-          {{ title }}
-        </p>
-      </header>
-      <div class="card-content">
-        <div class="is-content has-text-centered">
-          <b-icon :icon="icon" size="is-large" type="is-primary" />
+  <div class="card is-rounded">
+    <div class="card-image" v-if="image">
+      <figure class="image is-4by3">
+        <img
+          :src="image"
+          alt
+        />
+      </figure>
+    </div>
+    <div class="card-content">
+      <div class="media">
+        <div class="media-left" v-if="authorPic">
+          <figure class="image is-48x48">
+            <img
+              :src="authorPic"
+              alt
+            />
+          </figure>
+        </div>
+        <div class="media-content">
+          <p class="title is-4">{{title}}</p>
+          <p class="subtitle is-6" v-if="username">@{{username}}</p>
         </div>
       </div>
-      <footer class="card-footer">
-        <div class="card-footer-item">
-          <span>
-            <slot />
-          </span>
-        </div>
-      </footer>
+
+      <div class="content">
+        <slot></slot>
+      </div>
     </div>
   </div>
 </template>
@@ -29,10 +38,9 @@ export default {
       type: String,
       required: true
     },
-    icon: {
-      type: String,
-      required: true
-    }
+    image: String,
+    authorPic: String,
+    username: String,
   }
 }
 </script>
