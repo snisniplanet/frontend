4843aaa3a7bb6a12b72ecde88553415f48d1044f
Mattia Sinisi <mattia.sinisi@virgilio.it>
2020-06-09 13:06:37 +0200

    Added custom ProseMirror schema

---
+++ assets/schemas/prosemirror-schema-rich.js
@@ -0,0 +1,43 @@
+import { schema } from 'prosemirror-schema-basic'
+
+let custom = {
+  nodes: {
+    doc: { content: 'block+' }, // block+ means optionally group block nodes
+    paragraph: {
+      group: 'block',
+      content: 'inline*',
+      marks: '_',
+      toDOM() {
+        return ['p', 0]
+      },
+      parseDOM: [{tag: "p"}]
+    },
+    heading: { group: 'block', content: 'text*', marks: '' },
+    text: { group: "inline" }
+  },
+  marks: {
+    ...schema.spec.marks,
+    bold: {
+      parseDOM: [
+        { tag: 'strong' },
+        {
+          tag: 'b',
+          getAttrs: (node) => node.style.fontWeight != 'normal' && null
+        }
+      ],
+      toDOM() {
+        return ['b']
+      }
+    },
+    italic: {
+      parseDOM: [
+        { tag: 'i' },
+      ],
+      toDOM() {
+        return ['i']
+      }
+    }
+  }
+}
+
+export default Object.assign(schema, custom)
