c0a6560718b56389ea31ce5064731643bc05d546
Mattia Sinisi <mattia.sinisi@virgilio.it>
2020-06-11 12:13:12 +0200

    Fixed layout

--- components/atoms/Banner.vue
+++ components/atoms/Banner.vue
@@ -1,17 +1,23 @@
 <template>
-  <div class="hero is-small">
-    <div cass="hero-body">
-      <div class="section container">
+  <div class="hero">
+    <div class="hero-body">
+      <div class="container section">
         <h1 class="title is-1">
           <slot name="title"></slot>
         </h1>
-
-        <h3>
+        <h2 class="subtitle is-4">
           <slot name="subtitle"></slot>
-        </h3>
+        </h2>
 
         <slot></slot>
       </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+.section {
+  padding-left: 0;
+  padding-right: 0;
+}
+</style>
