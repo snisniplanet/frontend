d6c143ffc8ed138d5b8bf723ca7365f0de1f970f
Mattia Sinisi <mattia.sinisi@virgilio.it>
2020-06-15 12:50:01 +0200

    Test user constructor validation

--- test/User.test.js
+++ test/User.test.js
@@ -1,10 +1,35 @@
 import User from '@/classes/User'
-import { validate } from "class-validator";
+import { validate } from "class-validator"
 
-test('creates a new User instance', () => {
-  let user = new User('username', 'email@example.com', '312441h2hgs');
+function onlyWithConstraint(errors, constraint){
+  return errors.filter(item => {
+    return (item.constraints && item.constraints[constraint])
+  })
+}
+
+test('Creates a new User instance', () => {
+  let user = new User('username', 'email@example.com', '312441h2hgs')
 
   validate(user).then(errors => {
     expect(errors.length).toBe(0)
+  })
+})
+
+test('Invalid email errors', () => {
+  let user = new User('username', 'invalid email', '312441h2hgs')
+
+  validate(user).then(errors => {
+    errors = onlyWithConstraint(errors, 'isEmail')
+
+    expect(errors.length > 0).toBe(true)
+  })
+})
+
+test('Missing parameters errors', () => {
+  let user = new User('username')
+
+  validate(user).then(errors => {
+    errors = onlyWithConstraint(errors, 'isNotEmpty')
+    expect(errors.length > 1).toBe(true)
   })
 })
