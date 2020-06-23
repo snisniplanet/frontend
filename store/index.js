1ca8134e0e071d7122fe54541a29967af1e79c9d
Mattia Sinisi <mattia.sinisi@virgilio.it>
2020-06-23 23:12:21 +0200

    Only get user if token is present

--- store/index.js
+++ store/index.js
@@ -3,6 +3,7 @@ export const actions = {
     let tokenCookie = req.headers.cookie
 
     let token = tokenCookie ? tokenCookie.split("=")[1] : tokenCookie
-    await dispatch('user/get', token)
+    if (token)
+      await dispatch('user/get', token)
   }
 }
