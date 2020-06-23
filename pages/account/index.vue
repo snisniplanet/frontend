f2d08c439702534b6f05b9648e94dd5e5ae2a1b9
Mattia Sinisi <mattia.sinisi@virgilio.it>
2020-06-23 23:18:32 +0200

    Added basic account page

---
+++ pages/account/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <div class="section">
+      <div class="box has-shadow-light">
+        <p>Welcome <strong>{{user.username}}</strong>!</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import {mapGetters} from 'vuex'
+
+export default {
+  middleware: 'auth',
+  computed: mapGetters({
+    user: "user/all"
+  })
+}
+</script>
